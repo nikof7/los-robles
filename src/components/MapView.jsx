c2963a1bd127262f865a07b2393f3c5cfcb0ce39
nikof7 <wjuice7@gmail.com>
2025-03-31 17:08:23 -0300

    add tap={false} in mapview

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -5,7 +5,7 @@ const MapView = () => {
   const position = [-34.739726, -56.222658]; // Coordenadas (ejemplo: Montevideo, Uruguay)
 
   return (
-    <MapContainer center={position} zoom={14} style={{ height: "400px", width: "100%" }}>
+    <MapContainer center={position} zoom={14} style={{ height: "400px", width: "100%" }} tap={false}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
