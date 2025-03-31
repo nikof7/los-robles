462f48039c60583fc8b74f0e9b77de10c92d140a
nikof7 <wjuice7@gmail.com>
2025-03-31 17:24:55 -0300

    fix map icon

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,15 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
+
 import "leaflet/dist/leaflet.css";
 
 const MapView = () => {
   const position = [-34.739726, -56.222658]; // Coordenadas (ejemplo: Montevideo, Uruguay)
-
   return (
-    <MapContainer center={position} zoom={14} style={{ height: "400px", width: "100%" }} tap={false}>
+    <MapContainer center={position} zoom={14} style={{ height: "400px", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
         <Popup>
           Fraccionamiento Los Robles
         </Popup>
