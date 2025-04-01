30b03b8b6da0068ea7093503bd9a1757592c15b1
nikof7 <wjuice7@gmail.com>
2025-04-01 10:57:35 -0300

    Fix form on touch screen

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,32 +1,32 @@
 export default function ContactForm() {
   return (
-    <form action="https://formsubmit.co/wjuice7@gmail.com" method="POST" className="max-w-md mx-auto mt-6 space-y-4">
+    <form action="https://formsubmit.co/wjuice7@gmail.com" method="POST" className="max-w-md mx-auto mt-6 space-y-4 relative z-10">
       <input
         type="text"
         name="nombre"
         placeholder="Nombre"
         required
-        className="w-full p-2 border border-[var(--primary-color)] rounded-md focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)]"
+        className="w-full p-2 border border-[var(--primary-color)] rounded-md "
       />
       <input
         type="email"
         name="email"
         placeholder="Email"
         required
-        className="w-full p-2 border border-[var(--primary-color)] rounded-md focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)]"
+        className="w-full p-2 border border-[var(--primary-color)] rounded-md "
       />
       <input
         type="tel"
         name="telefono"
         placeholder="Teléfono"
         required
-        className="w-full p-2 border border-[var(--primary-color)] rounded-md focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)]"
+        className="w-full p-2 border border-[var(--primary-color)] rounded-md "
       />
       <textarea
         name="mensaje"
         placeholder="Mensaje"
         required
-        className="w-full p-2 border border-[var(--primary-color)] rounded-md h-24 resize-none focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)]"
+        className="w-full p-2 border border-[var(--primary-color)] rounded-md h-24 resize-none "
       ></textarea>
       <button type="submit" className="p-3 text-white py-2 rounded-lg bg-[var(--primary-color)]">
         Enviar mensaje
